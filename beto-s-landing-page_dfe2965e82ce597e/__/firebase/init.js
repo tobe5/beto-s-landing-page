248a757c1d409dc21919d285de3a1970070bf2cf
Beto <albcambro25@gmail.com>
2026-05-10 00:43:21 -0600

    downloaded landing page and modified

---
+++ beto-s-landing-page_dfe2965e82ce597e/__/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyBpszYfAog1tRRq_T3WsJHJ0nvrRrc3FbA",
+  "authDomain": "beto-s-landing-page.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "665907599334",
+  "projectId": "beto-s-landing-page",
+  "storageBucket": "beto-s-landing-page.firebasestorage.app"
+});
